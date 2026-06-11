067ee1d1cdf5efe4d31b4cd9ae13c5b0fe585934
rvalencia <rva.valencia@gmail.com>
2026-06-11 17:05:08 -0400

    feat: Se agregaron variables entorno para correo

--- ejemplo_correo/settings.py
+++ ejemplo_correo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,3 +117,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# campos para email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'pdjango44@gmail.com'
+EMAIL_HOST_PASSWORD = 'grdu flyx optb tdlb'
+
+DEFAULT_FROM_EMAIL = 'Test Django <pdjango44@gmail.com>'
